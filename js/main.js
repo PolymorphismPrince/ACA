54d0b3be36e8e85a49c0651eb250f1be47aab4c0
PolymorphismPrince <adenpowercow@gmail.com>
2018-10-09 20:53:00 +1100

    Made a panda army that follows the mouse

--- js/main.js
+++ js/main.js
@@ -39,14 +39,111 @@ function loadSprites () {
         pandaSprites[i-1].height = 390 / sizeScalar;
     }
 }
-var sizeScalar = 10;
+var sizeScalar = 4;
 var cowSprites = [];
 var pandaSprites = [];
+var cowPoses = [{sprite:0,speed:1,time:0.5},{sprite:2,speed:1,time:0.5}];
+var pandaPoses = [{sprite:0,speed:1,time:0.3},{sprite:1,speed:1,time:0.3},{sprite:2,speed:1,time:0.3},{sprite:1,speed:1,time:0.3}];
+
+
+class Animal {
+    //Type is true or false (panda or cow)
+    constructor (x,y,type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.movementCounter = 0;
+        this.sprites = (type) ? pandaSprites : cowSprites;
+        this.poses = (type) ? pandaPoses : cowPoses;
+        this.velocityScalar;
+        this.currentSpeed = 1;
+        this.rot = 0;
+    }
+    get xV () {
+        return Math.cos(this.radRot);
+    }
+    get yV () {
+        return Math.sin(this.radRot);
+    }
+
+    get radRot () {
+        return this.rot/180 * Math.PI;
+    }
+
+    get test () {
+        return [Math.atan2(this.yV,this.xV),this.radRot];
+    }
+    //Update pose and stuff
+    animate () {
+        //Increment movement counter
+        this.movementCounter++;
+        //Calculate time in seconds
+        var time = this.movementCounter / fps;
+        let timeAccumulator = 0;
+        //Itterate through the poses array
+        for (let pose of this.poses) {
+            //Add to the time accumulator
+            timeAccumulator += pose.time;
+            //Check if this is the right sprite
+            if (time < timeAccumulator) {
+                //Update the speed of the animal at this time
+                this.currentSpeed = pose.speed;
+                //Render the current animation of the movement on the canvas
+                draw(this.sprites[pose.sprite],this.x,this.y,this.rot + 70);
+                return;
+            }
+        }
+        //Return 0 so that the animation restarts
+        this.movementCounter = 0;
+        this.animate();
+    }
+    //Moving
+    move () {
+        var xDist = - this.x + pandaGoalX;
+        var yDist =  - this.y + pandaGoalY;
+        if (Math.sqrt(Math.pow(xDist,2) + Math.pow(yDist,2)) > 20) {
+            this.rot = Math.atan2(yDist,xDist) / (2 * Math.PI) * 360;
+        }
+        //Add to x and y
+        this.x += this.xV * this.currentSpeed;
+        this.y += this.yV * this.currentSpeed;
+    }
+    //Update everything
+    update () {
+        this.move();
+        this.animate();
+    }
+
+}
+var pandaGoalX = 500;
+var pandaGoalY = 500;
+document.body.onclick = function(event) {
+    pandaGoalX = event.clientX;
+    pandaGoalY = event.clientY;
+}
+
+//Load the Sprites
 loadSprites();
 
+var pandas = [];
+
+//Starting Code
 pandaSprites[2].onload = 
 function () {
-    draw(pandaSprites[0],0,0,1);
-    img = pandaSprites[0]
+    for (let i = 0; i < 10; i++) {
+        pandas.push(new Animal(Math.random() * 1000,0,true));
+    }
+    setInterval(update,1000/fps);
 } 
+
+const fps = 25;
+
+function update () {
+    ctx.fillStyle = "gray";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    for (let panda of pandas) {
+        panda.update();
+    }
+}
+
 
